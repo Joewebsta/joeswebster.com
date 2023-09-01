44603e0463c2535eb20792b7b5f5054d403a4f97
Joe Webster <joseph.scott.webster@gmail.com>
2023-09-01 17:59:36 -0400

    Install vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import FeaturedProject from "./components/FeaturedProject";
 import Footer from "./components/Footer";
+import { inject } from "@vercel/analytics";
+
+inject();
+
 import "./styles/reset.css";
 
 function App() {
